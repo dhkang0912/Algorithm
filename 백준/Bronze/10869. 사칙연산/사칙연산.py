2dd8956730b32e9098186081b5dc5d54f9fd3b73
dhkang0912 <152072710+dhkang0912@users.noreply.github.com>
2024-01-23 23:17:44 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,12 @@
+input = list(map(int, input().split()))
+plus = input[0] + input[1]
+minus = input[0] - input[1]
+multiple = input[0] * input[1]
+divide = int(input[0] / input[1])
+share = input[0] % input[1]
+
+print(plus)
+print(minus)
+print(multiple)
+print(divide)
+print(share)
