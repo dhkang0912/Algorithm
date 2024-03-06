6d6fe10b80ee8a5cadf089b71df9771a0997e590
dhkang0912 <152072710+dhkang0912@users.noreply.github.com>
2024-03-06 23:35:37 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -6,9 +6,8 @@ for i in range(N-2):
     for j in range(i+1, N-1):
         for k in range(j+1, N):
             card_sum = lst[i] + lst[j] + lst[k] # 3개를 뽑고 더한 값
-            if card_sum <= M : # 더한 값이 M을 넘으면 안됨
-                if card_max_sum < card_sum:
-                    card_max_sum = card_sum
+            if card_sum <= M and card_max_sum < card_sum: # 더한 값이 M을 넘으면 안됨
+                card_max_sum = card_sum
 
 print(card_max_sum)
 
