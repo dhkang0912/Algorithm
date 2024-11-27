280a3b4f1707d01566544b6965e6c718bc093e0f
dhkang0912 <152072710+dhkang0912@users.noreply.github.com>
2024-11-28 00:25:01 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,7 +1,11 @@
+# def solution(a, b):
+#     answer = 0
+#     if (int(str(a)+str(b)) > int(str(b)+str(a))):
+#         answer = int(str(a)+str(b))
+#     else :
+#         answer = int(str(b)+str(a))
+#     return answer
+
 def solution(a, b):
-    answer = 0
-    if (int(str(a)+str(b)) > int(str(b)+str(a))):
-        answer = int(str(a)+str(b))
-    else :
-        answer = int(str(b)+str(a))
+    answer = max(int(f'{a}{b}'),int(f'{b}{a}'))
     return answer
