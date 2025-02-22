afa6835f42f83e48739dc3499b2312b8566e93fb
dhkang0912 <152072710+dhkang0912@users.noreply.github.com>
2025-02-22 17:36:01 +0900

    [Silver I] Title: 정수 삼각형, Time: 256 ms, Memory: 26688 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,17 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[][] triangle = new int[n][n];
 
         for (int i = 0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j=0; j<=i;j++){
-                triangle[i][j] = sc.nextInt();
+                triangle[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        sc.close();
+        br.close();
         System.out.println(dp(n, triangle));
     }
 
