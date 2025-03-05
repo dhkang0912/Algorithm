241dd3fb80522a3b58b1732103e1c0c959f6ce2f
dhkang0912 <152072710+dhkang0912@users.noreply.github.com>
2025-03-05 14:35:42 +0900

    [Silver I] Title: 트리 순회, Time: 100 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    Map<String, Node> tree = new HashMap<>(); // 트리 저장
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.run();
+    }
+
+    public void run() throws IOException {
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N ; i++) {
+            String[] input = br.readLine().split(" "); // split을 사용하면 바로 배열에 넣을 수 있음
+            tree.put(input[0], new Node(input[1], input[2]));
+        }
+
+        preorder("A");
+        System.out.println();
+        inorder("A");
+        System.out.println();
+        postorder("A");
+        System.out.println();
+
+    }
+
+    void preorder(String node){
+        if(node.equals(".")) return;
+        System.out.print(node);
+        preorder(tree.get(node).left);
+        preorder(tree.get(node).right);
+    }
+
+    void inorder(String node){
+        if (node.equals(".")) return;
+        inorder(tree.get(node).left);
+        System.out.print(node);
+        inorder(tree.get(node).right);
+    }
+
+    void postorder(String node){
+        if (node.equals(".")) return;
+        postorder(tree.get(node).left);
+        postorder(tree.get(node).right);
+        System.out.print(node);
+    }
+
+
+    class Node {
+        String left, right;
+        Node(String left, String right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
